b2363c7f53cc2228287b5f38457ecb0cb1d61b22
Ruurd Bijlsma <ruurd@bijlsma.dev>
2025-04-28 12:49:53 +0200

    Use line plot and add screenshot for readme.

--- examples/graph_data.rs
+++ examples/graph_data.rs
@@ -9,7 +9,7 @@
 use std::error::Error;
 
 use meteostat::{Frequency, LatLon, Meteostat, MeteostatFrameFilterExt};
-use plotlars::{Axis, Legend, Line, Plot, Rgb, Shape, Text, TimeSeriesPlot};
+use plotlars::{Axis, Line, LinePlot, Plot, Rgb, Text};
 use polars::prelude::*;
 
 #[tokio::main]
@@ -40,36 +40,35 @@ async fn main() -> Result<(), Box<dyn Error>> {
     Ok(())
 }
 
-// --- Plotting Helper Function ---
-
-/// Plots the temperature from the DataFrame's 'datetime' and 'temp' columns.
-fn plot_temperature(data: &DataFrame) {
-    TimeSeriesPlot::builder()
-        .data(&data)
+fn plot_temperature(dataset: &DataFrame) {
+    LinePlot::builder()
+        .data(&dataset)
         .x("date")
-        .y("tmax")
-        .additional_series(vec!["tmin"])
-        .size(8)
-        .colors(vec![Rgb(235, 117, 0), Rgb(69, 157, 230)])
-        .lines(vec![Line::Dash, Line::Solid])
-        .with_shape(true)
-        .shapes(vec![Shape::Circle, Shape::Square])
-        .plot_title(Text::from("Meteostat Data").font("Arial").size(18))
-        .legend(&Legend::new().x(0.05).y(0.9))
-        .x_title("x")
-        .y_title(Text::from("y").color(Rgb(0, 0, 0)))
-        .y_title2(Text::from("y2").color(Rgb(0, 0, 0)))
-        .y_axis(
-            &Axis::new()
-                .value_color(Rgb(0,0, 0))
-                .show_grid(false)
-                .zero_line_color(Rgb(0, 0, 0)),
+        .y("tavg")
+        .additional_lines(vec!["tmin", "tmax"])
+        .colors(vec![
+            Rgb(120, 120, 120), // tavg
+            Rgb(69, 143, 196),  // tmin
+            Rgb(199, 115, 42),  // tmax
+        ])
+        .lines(vec![Line::Solid, Line::Dot, Line::Dot])
+        .width(3.0)
+        .plot_title(
+            Text::from("Temperature at De Bilt (2023)")
+                .font("Arial")
+                .size(18),
         )
-        .y_axis2(
+        .x_axis(
             &Axis::new()
-                .axis_side(plotlars::AxisSide::Right)
-                .value_color(Rgb(0,0, 0))
-                .show_grid(false),
+                .tick_values(
+                    (0..12)
+                        .map(|i| 19358.0 + i as f64 * (19722.0 - 19358.0) / 11.0)
+                        .collect(),
+                )
+                .tick_labels(vec![
+                    "Jan", "Feb", "Mar", "April", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov",
+                    "Dec",
+                ]),
         )
         .build()
         .plot();
