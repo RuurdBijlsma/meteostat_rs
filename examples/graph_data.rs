98d111620626bf86c926869348a330604164f1ff
Ruurd Bijlsma <ruurd@bijlsma.dev>
2025-05-02 17:00:50 +0200

    Update graph example.

--- examples/graph_data.rs
+++ examples/graph_data.rs
@@ -9,7 +9,7 @@
 use std::error::Error;
 
 use meteostat::{LatLon, Meteostat, Year};
-use plotlars::{Axis, Line, LinePlot, Plot, Rgb, Text};
+use plotlars::{Line, Plot, Rgb, TimeSeriesPlot};
 use polars::prelude::*;
 
 #[tokio::main]
@@ -41,35 +41,22 @@ async fn main() -> Result<(), Box<dyn Error>> {
 }
 
 fn plot_temperature(dataset: &DataFrame) {
-    LinePlot::builder()
+    TimeSeriesPlot::builder()
         .data(dataset)
         .x("date")
         .y("tavg")
-        .additional_lines(vec!["tmin", "tmax"])
+        .additional_series(vec!["tmin", "tmax"])
         .colors(vec![
             Rgb(120, 120, 120), // tavg
             Rgb(69, 143, 196),  // tmin
             Rgb(199, 115, 42),  // tmax
         ])
-        .lines(vec![Line::Solid, Line::Dot, Line::Dot])
-        .width(3.0)
-        .plot_title(
-            Text::from("Temperature at De Bilt (2023)")
-                .font("Arial")
-                .size(18),
-        )
-        .x_axis(
-            &Axis::new()
-                .tick_values(
-                    (0..12)
-                        .map(|i| 19358.0 + i as f64 * (19722.0 - 19358.0) / 11.0)
-                        .collect(),
-                )
-                .tick_labels(vec![
-                    "Jan", "Feb", "Mar", "April", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov",
-                    "Dec",
-                ]),
-        )
+        .lines(vec![
+            Line::Solid,
+            Line::Dot,
+            Line::Dot,
+        ])
+        .plot_title("Temperature at De Bilt (2023)")
         .build()
         .plot();
 }
